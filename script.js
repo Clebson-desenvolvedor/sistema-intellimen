498a357c3183691409736869d0fbc842e9107caa
Clebson Araújo <clebson.araujo.25@gmai.com>
2021-02-25 23:48:19 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function hideDiv(){
+    document.getElementById('fazIn').style.display="none";
+    document.getElementById('parouIn').style.display="none";
+}
+
+function showDivYes(){
+    document.getElementById('fazIn').style.display="block"
+    document.getElementById('parouIn').style.display="none"
+}
+
+function showDivStop(){
+    document.getElementById('parouIn').style.display="block"
+    document.getElementById('fazIn').style.display="none"
+}
